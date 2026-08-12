d6a13cee954842ac0f6c7741a0d92de65f0b5370
Babar Rasika <109503117+Rasika-Babar@users.noreply.github.com>
2026-08-12 17:54:15 +0530

    Update streamlit_app.py
    ading pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,11 @@ st.write("The name on your smoothie will be", name_of_order)
 
 
 my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()                                                                       
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()           
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredient_list=st.multiselect(
     'Choose upto 5 diffrent ingredients:'
